72564bd463a132ef8e6b36b06434785e2cc3d130
BrandonMunoz <soldadodelcaos10@gmail.com>
2019-01-15 00:33:14 -0500

    Semana3Dia4
    Ejercicios con arrays C# Visual Studio

---
+++ Semana03/Dia4/S3D14/ejercicio01/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ejercicio01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        static int [] llenarArray(int tamaño)
+        {
+            int[] array = new int[tamaño];
+            int = indice=0
+
+            for (int i = 0; i<indice; i++)
+            {
+                if (i % 2 == 0)
+                    array[indice] = i;
+                    indice++;
+            }
+        }
+
+
+    }
+}
